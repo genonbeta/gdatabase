e12fd00ed68a9b75d67fd4a26f184cb2d5f60af8
Veli <veli.tasali@gmail.com>
2018-06-06 00:03:33 +0300

    First commit

---
+++ gdatabase/src/main/java/com/genonbeta/android/database/SQLType.java
@@ -0,0 +1,17 @@
+package com.genonbeta.android.database;
+
+/**
+ * created by: Veli
+ * date: 10.10.2017 17:37
+ */
+
+public enum SQLType
+{
+	INTEGER,
+	DOUBLE,
+	LONG,
+	TEXT,
+	DATE,
+	BLOB,
+	TIMESTAMP
+}
